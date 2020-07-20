125b3b20f06539c09fb8e0d56e0c089694474c17
nirmit jasapara <nirmit.jasapara@sjsu.edu>
2020-07-20 15:14:27 -0700

    added router

---
+++ src/store.js
@@ -0,0 +1,9 @@
+const bookmarks = [
+    { id: uuid(),
+      title: 'Test1',
+      url: 'https://www.url.com',
+      description: 'Ipsum',
+      rating: 1 }
+  ]
+
+  module.exports = { bookmarks }
